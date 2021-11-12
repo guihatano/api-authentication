f9c860a4e147b2258235661bf6ac07a079c9643e
Guilherme Y. Hatano <guihatano@gmail.com>
2021-11-11 21:41:43 -0300

    add request spec for application controller

---
+++ spec/requests/application_controller_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe(ApplicationController, type: :request, clean: true, order: :random) do
+  describe 'GET /' do
+    it 'successful get response 200 OK' do
+      get '/'
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('Up and running!')
+    end
+  end
+end
