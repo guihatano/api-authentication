136ff660ab6029ccb50ebc3771bf284813c29359
Guiherme Y. Hatano <guilherme.hatano@libercapital.com.br>
2022-01-26 16:57:15 -0300

    add gem chusaku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  # @route GET /api/user {format: :json} (user)
   def show; end
 
+  # @route PATCH /api/user {format: :json} (user)
+  # @route PUT /api/user {format: :json} (user)
   def update
     if current_user.update(user_params)
       render :show
