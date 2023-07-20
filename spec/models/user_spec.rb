f7c3a7d29315ad44bfac05bca3549ea6d34a5ba7
Guilherme Y. Hatano <guihatano@gmail.com>
2023-07-19 22:45:24 -0300

    refactor spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,23 +4,25 @@ require 'rails_helper'
 
 RSpec.describe(User, type: :model, order: :random) do
   subject(:user) do
-    described_class.new(email: 'email@test.com',
-                        password: 'password',
-                        name: 'User Test',
-                        image: nil)
+    described_class.new(
+      email: 'email@test.com',
+      password: 'password',
+      name: 'User Test',
+      image: nil
+    )
   end
 
   it 'is valid with valid attributes' do
-    expect(user).to be_valid
+    expect(user).to(be_valid)
   end
 
   it 'is not valid without an email' do
     user.email = nil
-    expect(user).not_to be_valid
+    expect(user).not_to(be_valid)
   end
 
   it 'is not valid without a password' do
     user.password = nil
-    expect(user).not_to be_valid
+    expect(user).not_to(be_valid)
   end
 end
